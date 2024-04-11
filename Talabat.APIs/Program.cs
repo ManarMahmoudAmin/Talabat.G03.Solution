72fd406f96f16f6ad07e7fafe84b97cb7bfac031
ManarMahmoudAmin <manareltayeb825@gmail.com>
2024-04-11 21:50:31 +0200

    Data Seeding

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -39,6 +39,8 @@ namespace Talabat.APIs
             try
             {
                 await _dbContext.Database.MigrateAsync();
+                await StoreContextSeed.SeedAsync(_dbContext);
+
             }
             catch (Exception ex)
             {
