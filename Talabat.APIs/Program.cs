2b9d465a8508df4fda154abdf06e516d31f8bba5
ManarMahmoudAmin <manareltayeb825@gmail.com>
2024-04-17 09:57:53 +0200

    Handling Not Found EndPoint

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -86,6 +86,7 @@ namespace Talabat.APIs
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            app.UseStatusCodePagesWithReExecute("errors/{0}");
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
