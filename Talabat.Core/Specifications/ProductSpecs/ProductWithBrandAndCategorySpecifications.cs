71cad622ee7ae498037342c0e845449ff1a0c9da
ManarMahmoudAmin <manareltayeb825@gmail.com>
2024-04-20 07:12:48 +0200

    Filtration

--- Talabat.Core/Specifications/ProductSpecs/ProductWithBrandAndCategorySpecifications.cs
+++ Talabat.Core/Specifications/ProductSpecs/ProductWithBrandAndCategorySpecifications.cs
@@ -9,7 +9,11 @@ namespace Talabat.Core.Specifications.ProductSpecs
 {
     public class ProductWithBrandAndCategorySpecifications :BaseSpecifications<Product>
     {
-        public ProductWithBrandAndCategorySpecifications(string sort):base()
+        public ProductWithBrandAndCategorySpecifications(string sort, int? brandId, int? categoryId)
+                    : base(P =>
+                            (!brandId.HasValue || P.BrandId == brandId.Value) &&
+                            (!categoryId.HasValue || P.CategoryId == categoryId.Value)
+                         )
         {
             Includes.Add(P => P.Brand);
             Includes.Add(P => P.Category);
