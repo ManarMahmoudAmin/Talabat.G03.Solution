a022efc956b479a1dd1afa6c98819f1b933b17c7
ManarMahmoudAmin <manareltayeb825@gmail.com>
2024-04-23 08:10:59 +0200

    Basket Module Entities

---
+++ Talabat.Core/Entities/Basket/CustomerBasket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.Basket
+{
+    public class CustomerBasket
+    {
+        public string Id { get; set; } = null!;
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+        public CustomerBasket(string id) 
+        {
+            Id = id;
+        }
+    }
+}
