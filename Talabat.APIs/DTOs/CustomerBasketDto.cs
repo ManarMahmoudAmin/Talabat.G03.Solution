2306091689f759105dde683ac773d4e0ed974eaf
ManarMahmoudAmin <manareltayeb825@gmail.com>
2024-04-23 13:57:13 +0200

    Basket Controller

---
+++ Talabat.APIs/DTOs/CustomerBasketDto.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.APIs.Controllers;
+using Talabat.APIs.Errors;
+using Talabat.Core.Entities.Basket;
+using Talabat.Core.Repositories.Contract;
+
+namespace Talabat.APIs.DTOs
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet] // Get: /api/basket/id
+        public async Task<ActionResult<CustomerBasket>> GetBasket(string id)
+        {
+            var basket = await _basketRepository.GetBasketAsync(id);
+            if (basket is null) return Ok(new CustomerBasket(id));
+            return Ok(basket);  
+        }
+
+        [HttpPost] // post : /api/basket
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        {
+            var createdOrUpdatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+            if (createdOrUpdatedBasket is null) return BadRequest(new ApiResponse(400));
+            return Ok(createdOrUpdatedBasket);
+        }
+
+        [HttpDelete] //DELETE : /api/basket
+        public async Task DeleteBasket(string id)
+        {
+            await _basketRepository.DeleteBasketAsync(id);
+        }
+    }
+}
