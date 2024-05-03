bc91790f42a4ca0c20f74c790fcecc6367811545
ManarMahmoudAmin <manareltayeb825@gmail.com>
2024-05-03 20:09:30 +0300

    Get Current User

--- Talabat.APIs/Controllers/AccountController.cs
+++ Talabat.APIs/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using Talabat.APIs.DTOs;
 using Talabat.APIs.Errors;
 using Talabat.Core.Entitites.Identity;
@@ -58,6 +60,22 @@ namespace Talabat.APIs.Controllers
 			{
 				DisplayName = user.DisplayName,
 				Email = user.Email,
+				Token = await authService.CreateTokenAsync(user, userManager)
+			});
+		}
+
+		[Authorize]
+		[HttpGet] // GET /api/Account
+		public async Task<ActionResult<UserDto>> GetCurrentUser()
+		{
+			var email = User.FindFirstValue(ClaimTypes.Email) ?? string.Empty;
+
+			var user = await userManager.FindByEmailAsync(email);
+
+			return Ok(new UserDto()
+			{
+				DisplayName = user.DisplayName ?? string.Empty,
+				Email = user.Email ?? string.Empty,
 				Token = await authService.CreateTokenAsync(user, userManager)
 			});
 		}
