044b742cebd3c92e5879e6f7481fbf5faec66923
ManarMahmoudAmin <manareltayeb825@gmail.com>
2024-05-04 20:18:00 +0300

    Order Module - Entities

---
+++ Talabat.Core/Entitites/Order_Aggregate/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Core.Entitites.Order_Aggregate
+{
+	public class OrderItem : BaseEntity
+	{
+		public ProductItemOrdered Product { get; set; } = null!;
+		public int Quantity { get; set; }
+		public decimal Price { get; set; }
+	}
+}
