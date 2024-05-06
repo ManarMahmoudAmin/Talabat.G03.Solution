8b81ded4a7e41db3d377eea135fe2945b1acb114
ManarMahmoudAmin <manareltayeb825@gmail.com>
2024-05-06 07:50:09 +0300

    Unit Of Work

---
+++ Talabat.Core/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+
+namespace Talabat.Core
+{
+	public interface IUnitOfWork : IAsyncDisposable
+	{
+		public IGenaricRepository<T> Repository<T>() where T : BaseEntity;
+		public Task<int> Compelete();
+	}
+}
---
+++ Talabat.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Infrastructure._Data;
+
+namespace Talabat.Infrastructure
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly StoreContext _dbContext;
+		private Hashtable _repositories;
+
+		public UnitOfWork(StoreContext dbContext)
+		{
+			_dbContext = dbContext;
+			_repositories = new Hashtable();
+		}
+		public async Task<int> Compelete()
+			=> await _dbContext.SaveChangesAsync();
+
+
+		public async ValueTask DisposeAsync()
+		  => await _dbContext.DisposeAsync();
+
+		public IGenaricRepository<T> Repository<T>() where T : BaseEntity
+		{
+			var key = typeof(T).Name;
+			if (!_repositories.ContainsKey(key))
+			{
+				var repo = new GenericRepository<T>(_dbContext) as GenericRepository<BaseEntity>;
+				_repositories.Add(key, repo);
+			}
+			return (IGenaricRepository<T>)_repositories[key];
+		}
+	}
+}
